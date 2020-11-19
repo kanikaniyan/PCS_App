fde89749777737233acf7c5068da01aed96a4751
kanikaniyan <kkaniyan96@gmail.com>
2020-11-19 14:06:47 +0530

    commit

--- src/daoImpl/SkillDaoImpl.java
+++ src/daoImpl/SkillDaoImpl.java
@@ -13,12 +13,44 @@ public class SkillDaoImpl implements ISkillDao{
 	}
 	@Override
 	public void getAllSkills() {
-		// TODO Auto-generated method stub
+		try {
+			Statement stmt=conn.createStatement();
+			ResultSet rst=stmt.executeQuery("select * from skill");
+			if(rst!=null) {
+				Skill skill= new Skill();
+				while(rst.next()) {
+					skill.setSkillID(rst.getInt(1));
+					skill.setSkillName(rst.getString(2));
+					skill.setSkillDescription(rst.getString(3));
+					skill.setActive(rst.getString(4));
+					System.out.println(skill);
+				}
+			}
+		}
+		catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
+		
 		
 	}
 	@Override
 	public void addSkill(Skill skill) {
-		// TODO Auto-generated method stub
+		try {
+			PreparedStatement pst=conn.prepareStatement("insert into Skill values(?,?,?)");
+			pst.setString(1, skill.getSkillName());
+			pst.setString(2, skill.getSkillDescription());
+			pst.setString(3, skill.getActive());
+			int i=pst.executeUpdate();
+			if(i==1) {
+				System.out.println("1 record inserted...");
+			}
+			else {
+				System.out.println("insertion falied...");
+			}
+		}
+		catch (SQLException ex) {
+			System.out.println(ex.getMessage());
+		}
 		
 	}
 	@Override
